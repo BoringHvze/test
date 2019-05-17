40598045a661343ffcdb3ab2022a469623b6bbe0
Pavel <someweed11@gmail.com>
2019-05-17 17:35:21 +0300

    Первый гит

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/*let obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log (obj["age"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log (arr[0]);
+*/
+// alert("Hello world!");
+
+/*let answer = confirm("Are you here?");
+console.log(answer);*/
+
+/*let answer = prompt("18?", "Да");
+console.log(typeof(answer));*/
+
