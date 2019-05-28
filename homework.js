ca2586871bd7cd798d0602f90295a12a8aee829f
Pavel <someweed11@gmail.com>
2019-05-28 11:21:52 +0300

    изменения

---
+++ homework.js
@@ -0,0 +1,27 @@
+/*let x = 5;
+alert(x++);*/ // 5
+
+/*console.log([] + false - null + true);// NaN*/
+
+/*let y = 1;
+let x = y = 2;
+alert(x); // 2*/
+
+/*console.log([] + 1 + 2); // 12*/
+
+/*alert("1"[0]); // 1*/
+
+/*console.log(2 && 1 && null && 0 && undefined); //null */
+
+/*alert( null || 2 && 3 || 4 ); // 3*/
+
+/*let a = [1, 2, 3];
+    b = [1, 2, 3];  
+console.log(a == b); //false*/
+
+/*alert( +"Infinity" ); // Infinity */
+
+/*console.log("Ёжик" > "яблоко"); // false*/
+
+/*console.log(0 || "" || 2 || undefined || true || falsе); //2*/
+console.log(typeof(NaN));
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 /*let obj = {
     name: "John",
     age: 25,
@@ -17,3 +17,73 @@ console.log(answer);*/
 /*let answer = prompt("18?", "Да");
 console.log(typeof(answer));*/
 
+/*if (2*4 == 9) {
+    console.log("Верно!")
+} else {
+    console.log("Неверно!")
+}*/
+
+/*let num = 50;
+if (num <49) {
+    console.log("Worst!")
+} else if (num > 100) {
+    console.log("Too much")
+} else {
+    console.log("Thts right")
+}
+
+(num == 50) ? console.log("Thats right!") : console.log("Worst!");
+
+switch (num) {
+    case num < 49:
+        console.log("Worst!");
+        break;
+    case num > 100:  
+        console.log("Too much");  
+        break;
+    case 50:
+        console.log("Thats right!")
+        break;
+    default: 
+        console.log("Something wrong!");
+        break;           
+}*/
+/*let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD"),
+    questOne = prompt("Введите обязательную статью расходов в этом месяце"),
+    questTwo = prompt("Во сколько обойдется?");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}; 
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);*/
+
+/*let num = 50;
+while (num < 55) {
+    console.log(num);
+    num++;
+}*/
+/*do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+} */
